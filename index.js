4ff6534c63dfa6c1237f64e6ff25c7a8918e4d7a
Alexandru Constantin <dextor@gmail.com>
2015-03-04 23:33:12 +0200

    'first'

---
+++ index.js
@@ -0,0 +1,68 @@
+var fs = require('fs');
+var Entities = require('html-entities').XmlEntities; 
+var entities = new Entities();
+var request = require('request');
+var cheerio = require('cheerio');
+
+var domain = 'http://www.imdb.com';
+
+var url = domain + '/search/title?at=0&certificates=us%3Ag,us%3Apg,us%3Atv_14,us%3Atv_y7&genres=animation&num_votes=400,&title_type=tv_series';
+
+
+var currentPage = 1;
+var rank = 1;
+
+
+
+function requestPage(url) {
+    console.log("Page: " + currentPage, url);
+    request({ 'url': url,headers: { 'Accept-Language': 'en-US'    }  }, function(error, response, html) {
+
+        if (!error) {
+
+            var $ = cheerio.load(html, {
+                normalizeWhitespace: true,
+                xmlMode: false,
+                decodeEntities: false
+            });
+            if ($('A:contains(\'Next\')').length > 0) {
+
+		$(".detailed").each(function(){ 
+		        var $item = $(this);
+                        var id = $item.find('a').attr('href').split('/')[2];
+                        var $imgo = $item.find('img').attr('src');
+
+                        if($imgo.indexOf('.gif') === -1){
+                      
+                        var imga = $imgo.split('.');
+			var img =  imga.splice(0,imga.length-2).join('.') + '.jpg'; // get full image from cdn
+                        console.log(img);
+                        var writer = fs.createWriteStream(id+'.jpg');
+			request(img).pipe(writer);
+                        } 
+			var year = $item.find('.year_type').text().split(' ')[0].split('(').join('');  
+
+			var title = entities.decode(  $( $item.find('a').get(1) ).text()   )  ;
+			var plot = entities.decode(   $item.find('.outline').text()  )  ;
+
+
+                        //console.log(id); //rank,title,year, plot
+	                rank++;
+
+		});
+
+                var nextUrl = domain + $('A:contains(\'Next\')').last().attr('href');
+
+                currentPage++;
+                requestPage(nextUrl);
+            } else {
+                console.log('Done');
+                return;
+            }
+        } else {
+            console.log('Error!', error);
+        }
+    });
+}
+
+requestPage(url);
