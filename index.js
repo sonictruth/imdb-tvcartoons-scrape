2a09acd70c7e27b99670d877b1b02d7290a4d22c
Alexandru Constantin <alex@think002.vendio.ro>
2015-03-05 12:10:28 +0200

    c-c-c-changes

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var fs = require('fs');
-var Entities = require('html-entities').XmlEntities; 
+var util = require('util');
+var Entities = require('html-entities').XmlEntities;
 var entities = new Entities();
 var request = require('request');
 var cheerio = require('cheerio');
@@ -15,8 +16,13 @@ var rank = 1;
 
 
 function requestPage(url) {
-    console.log("Page: " + currentPage, url);
-    request({ 'url': url,headers: { 'Accept-Language': 'en-US'    }  }, function(error, response, html) {
+    //console.log("Page: " + currentPage, url);
+    request({
+        'url': url,
+        headers: {
+            'Accept-Language': 'en-US'
+        }
+    }, function(error, response, html) {
 
         if (!error) {
 
@@ -27,34 +33,35 @@ function requestPage(url) {
             });
             if ($('A:contains(\'Next\')').length > 0) {
 
-		$(".detailed").each(function(){ 
-		        var $item = $(this);
-                        var id = $item.find('a').attr('href').split('/')[2];
-                        var $imgo = $item.find('img').attr('src');
+                $(".detailed").each(function() {
+                    var $item = $(this);
+                    var id = $item.find('a').attr('href').split('/')[2];
+                    var imgo = $item.find('img').attr('src');
+                    /*
+                    if (imgo.indexOf('.gif') === -1) {
 
-                        if($imgo.indexOf('.gif') === -1){
-                      
-                        var imga = $imgo.split('.');
-			var img =  imga.splice(0,imga.length-2).join('.') + '.jpg'; // get full image from cdn
+                        var imga = imgo.split('.');
+                        var img = imga.splice(0, imga.length - 2).join('.') + '.jpg'; // get full image from cdn
                         console.log(img);
-                        var writer = fs.createWriteStream(id+'.jpg');
-			request(img).pipe(writer);
-                        } 
-			var year = $item.find('.year_type').text().split(' ')[0].split('(').join('');  
+                        var writer = fs.createWriteStream(id + '.jpg');
+                        request(img).pipe(writer);
+                    }
+                    */
+                    var year = $item.find('.year_type').text().split(' ')[0].split('(').join('');
+                    var title = entities.decode($($item.find('a').get(1)).text());
+                    var plot = entities.decode($item.find('.outline').text());
+                    var votes = $item.find('.rating.rating-list').first().attr('title').split('(')[1].split(' votes)')[0].split(',').join('');
+                    var ytsearch = 'https://www.youtube.com/results?search_query=' + encodeURIComponent(title);
+                    var csv = util.format('"%s","%s","%s","%s","%s","%s","%s"',votes, id,title, ytsearch,imgo,year,plot); 
+                    console.log(csv); //rank,title,year, plot
+                    rank++;
 
-			var title = entities.decode(  $( $item.find('a').get(1) ).text()   )  ;
-			var plot = entities.decode(   $item.find('.outline').text()  )  ;
-
-
-                        //console.log(id); //rank,title,year, plot
-	                rank++;
-
-		});
+                });
 
                 var nextUrl = domain + $('A:contains(\'Next\')').last().attr('href');
 
                 currentPage++;
-                requestPage(nextUrl);
+                //requestPage(nextUrl);
             } else {
                 console.log('Done');
                 return;
