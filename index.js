f89a636675d8fae0ee99dbe843e7de1293af1999
Alexandru Constantin <alex@think002.vendio.ro>
2015-03-05 12:53:35 +0200

    nextpage bugfix

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ var cheerio = require('cheerio');
 
 var domain = 'http://www.imdb.com';
 
-var url = domain + '/search/title?at=0&certificates=us%3Ag,us%3Apg,us%3Atv_14,us%3Atv_y7&genres=animation&num_votes=400,&title_type=tv_series';
+var url = domain + '/search/title?genres=animation&title_type=tv_series&num_votes=1000,&certificates=us:tv_g,us:tv_pg,us:tv_14,us:tv_y7,us:g,us:pg,us:14,us:y7';
 
 
 var currentPage = 1;
@@ -22,7 +22,7 @@ function requestPage(url) {
         headers: {
             'Accept-Language': 'en-US'
         }
-    }, function(error, response, html) {
+    }, function (error, response, html) {
 
         if (!error) {
 
@@ -31,39 +31,43 @@ function requestPage(url) {
                 xmlMode: false,
                 decodeEntities: false
             });
+
+
+            $(".detailed").each(function () {
+                var $item = $(this);
+                var id = $item.find('a').attr('href').split('/')[2];
+                var imgo = $item.find('img').attr('src');
+                /*
+                if (imgo.indexOf('.gif') === -1) {
+
+                    var imga = imgo.split('.');
+                    var img = imga.splice(0, imga.length - 2).join('.') + '.jpg'; // get full image from cdn
+                    console.log(img);
+                    var writer = fs.createWriteStream(id + '.jpg');
+                    request(img).pipe(writer);
+                }
+                */
+                var year = $item.find('.year_type').text().split(' ')[0].split('(').join('');
+                var title = entities.decode($($item.find('a').get(1)).text()).split('"').join('""');
+                var plot = entities.decode($item.find('.outline').text()).split('"').join('""'); // scape quotes csv
+                var votes = $item.find('.rating.rating-list').first().attr('title').split('(')[1].split(' votes)')[0].split(',').join('');
+                var ytsearch = 'https://www.youtube.com/results?search_query=' + encodeURIComponent(title);
+                var csv = util.format('"%s","%s","%s","%s","[PUT YT HERE]","%s","%s","%s"', votes, id, title, ytsearch, imgo, year, plot);
+                console.log(csv); //rank,title,year, plot
+                rank++;
+
+            });
+
+            var nextUrl = domain + $('A:contains(\'Next\')').last().attr('href');
+
+            currentPage++;
+
+
+
             if ($('A:contains(\'Next\')').length > 0) {
-
-                $(".detailed").each(function() {
-                    var $item = $(this);
-                    var id = $item.find('a').attr('href').split('/')[2];
-                    var imgo = $item.find('img').attr('src');
-                    /*
-                    if (imgo.indexOf('.gif') === -1) {
-
-                        var imga = imgo.split('.');
-                        var img = imga.splice(0, imga.length - 2).join('.') + '.jpg'; // get full image from cdn
-                        console.log(img);
-                        var writer = fs.createWriteStream(id + '.jpg');
-                        request(img).pipe(writer);
-                    }
-                    */
-                    var year = $item.find('.year_type').text().split(' ')[0].split('(').join('');
-                    var title = entities.decode($($item.find('a').get(1)).text());
-                    var plot = entities.decode($item.find('.outline').text());
-                    var votes = $item.find('.rating.rating-list').first().attr('title').split('(')[1].split(' votes)')[0].split(',').join('');
-                    var ytsearch = 'https://www.youtube.com/results?search_query=' + encodeURIComponent(title);
-                    var csv = util.format('"%s","%s","%s","%s","%s","%s","%s"',votes, id,title, ytsearch,imgo,year,plot); 
-                    console.log(csv); //rank,title,year, plot
-                    rank++;
-
-                });
-
-                var nextUrl = domain + $('A:contains(\'Next\')').last().attr('href');
-
-                currentPage++;
-                //requestPage(nextUrl);
+                requestPage(nextUrl);
             } else {
-                console.log('Done');
+                //console.log('Done');
                 return;
             }
         } else {
